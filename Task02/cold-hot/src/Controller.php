bf44afc4b7e8a3156ae22b14996e859bd0433941
Ivante2004 <tsybisov2004@mail.ru>
2024-10-05 21:14:26 +0300

    Task02

---
+++ Task02/cold-hot/src/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mario2003\ColdHot\Controller;
+
+use Mario2003\ColdHot\View;
+
+function startGame() {
+    View\showStartScreen();
+}
---
+++ Task02/cold-hot/src/View.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Mario2003\ColdHot\View;
+
+function showStartScreen() {
+    \cli\line("Welcome to Cold-hot!");
+}
